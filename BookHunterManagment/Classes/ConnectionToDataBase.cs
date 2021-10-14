5fa062924d65c2bcd6dd61115a8cd526f1e2bc02
LCVD <92400217+LCVD@users.noreply.github.com>
2021-10-14 21:31:38 +0500

    Add files via upload

---
+++ BookHunterManagment/Classes/ConnectionToDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BookHunterManagment.Classes
+{
+    class ConnectionToDataBase
+    {
+        public static MySqlConnection Connection()
+        {
+            string path = "server=localhost;user=root;password=1234;database=bookhunter_database";
+            MySqlConnection conn = new MySqlConnection(path);
+            return conn;
+        }
+    }
+}
+
---
+++ BookHunterManagment/Classes/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHunterManagment.Classes
+{
+    class Employee
+    {
+        public string code { get; set; }
+        public string username { get; set; }
+        public string surname { get; set; }
+        public string patronymic { get; set; }
+        public string phone { get; set; }
+    }
+}
