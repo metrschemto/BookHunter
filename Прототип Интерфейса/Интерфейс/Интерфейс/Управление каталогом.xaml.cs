82efb7550195ec7ace3d95d86c4bcaf327d8f937
metrschemto <92399851+metrschemto@users.noreply.github.com>
2021-10-14 21:33:41 +0500

    Add files via upload

---
+++ Прототип Интерфейса/Интерфейс/Интерфейс/Управление каталогом.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Интерфейс
+{
+    /// <summary>
+    /// Логика взаимодействия для Управление_каталогом.xaml
+    /// </summary>
+    public partial class Управление_каталогом : Window
+    {
+        public Управление_каталогом()
+        {
+            InitializeComponent();
+        }
+    }
+}
