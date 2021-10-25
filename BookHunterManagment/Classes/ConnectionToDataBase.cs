d91bbfa757053647b553b84fceb3a7e82477207c
metrschemto <92399851+metrschemto@users.noreply.github.com>
2021-10-25 23:59:13 +0500

    Add files via upload

---
+++ BookHunterManagment/Classes/ConnectionToDataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BookHunterManagment.Classes
+{
+    class ConnectionToDataBase
+    {
+        public static MySqlConnection Connection()
+        {
+            string path = "server=localhost;user=root;password=Powerdog132;database=bookhunter_database";
+            MySqlConnection conn = new MySqlConnection(path);
+            return conn;
+        }
+        public void QueryExecution(string Query, string Find, string Sort)
+        {
+            MySqlConnection conn = ConnectionToDataBase.Connection();
+            conn.Open();
+            string FinallyQuery = String.Format(Query, Find, Sort);
+            MySqlCommand command = new MySqlCommand(Query, conn);
+            command.ExecuteReader();
+        }
+    }
+}
+
