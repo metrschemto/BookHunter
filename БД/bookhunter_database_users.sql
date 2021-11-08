3c0035532e7913448f9cba803f2fe03af2f0c3be
metrschemto <92399851+metrschemto@users.noreply.github.com>
2021-11-08 23:03:44 +0500

    Add files via upload

---
+++ БД/bookhunter_database_users.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `bookhunter_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bookhunter_database`;
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bookhunter_database
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_code` int NOT NULL AUTO_INCREMENT,
+  `user_type` int NOT NULL,
+  `user_login` varchar(60) NOT NULL,
+  `user_password` varchar(60) NOT NULL,
+  `user_name` varchar(60) NOT NULL,
+  `user_phone` varchar(60) DEFAULT NULL,
+  `user_surname` varchar(60) DEFAULT NULL,
+  `user_patronymic` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`user_code`),
+  KEY `type_key_idx` (`user_type`),
+  CONSTRAINT `type_key` FOREIGN KEY (`user_type`) REFERENCES `user_types` (`type_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,0,'admin','admin','ADMINISTRATOR','89374888603','ADMINISTRATOR','ADMINISTRATOR'),(2,1,'user1','user1','Владимир','81578961991','Попов','Егорович'),(3,1,'user2','user2','Сергей','51715781788','Воробьев','Иванович'),(4,1,'user3','user3','Иван','81586171612','Скоробей','Петрович'),(5,1,'user4','user4','Арнольд','15781798921','Шварцнигер','Терминаторович'),(6,1,'user5','user5','Мустафа','17581927891','Алиба','Талибанович'),(7,1,'user6','user6','Евкакий','87992718921','Православный','Божий');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-08 22:56:47
