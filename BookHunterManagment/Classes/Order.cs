c7477319c333985966561c0c0f16ab76a972e92c
metrschemto <92399851+metrschemto@users.noreply.github.com>
2021-11-09 11:51:14 +0500

    Add files via upload

---
+++ BookHunterManagment/Classes/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+
+namespace BookHunterManagment.Classes
+{
+    class Order
+    {
+        public string OrderCode { get; set; }
+        public SolidColorBrush ColorLabel { get; set; }
+        public string OrderStatus { get; set; }
+        public string OrderCost { get; set; }
+        public bool IsEnableButton { get; set; }
+    }
+}
