d555258108aaaaa7a340a3aedb6939eaf89c24f0
metrschemto <92399851+metrschemto@users.noreply.github.com>
2021-11-09 11:52:36 +0500

    Add files via upload

---
+++ BookHunter/BookHunter/Classes/BookItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Threading.Tasks;
+
+namespace BookHunter.Classes
+{
+    class BookItem
+    {
+        public string ID { get; set; }
+        public string book_name { get; set; }
+        public string book_genre { get; set; }
+        public string book_author { get; set; }
+        public string books_in_storage { get; set; }
+        public string book_cost { get; set; }
+        public string publishing_house { get; set; }
+        public string age_restriction { get; set; }
+        public string publishing_year { get; set; }
+        public string pages_count { get; set; }
+        public string book_circulation { get; set; }
+        public BitmapImage bookimage { get; set; }
+    }
+}
---
+++ BookHunter/BookHunter/Windows/OrderView.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace BookHunter.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderView.xaml
+    /// </summary>
+    public partial class OrderView : Window
+    {
+        string OrderId;
+        public OrderView(string IdOrder)
+        {
+            OrderId = IdOrder;
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OrderContent.ItemsSource = Classes.ConnectionToDataBase.OrderItems(OrderId);
+            MySqlConnection connection = Classes.ConnectionToDataBase.Connection();
+            connection.Open();
+            string QueryCostOrder = "SELECT * FROM bookhunter_database.order where order_code = " + OrderId;
+            MySqlCommand command = new MySqlCommand(QueryCostOrder, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            CostOrder.Content = "Сумма: " + reader.GetString(2) + " руб.";
+            connection.Close();
+        }
+
+        private void DeleteBook_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.OrderItem obj = ((FrameworkElement)sender).DataContext as Classes.OrderItem;
+            string DeleteBookQuery = "Delete from bookhunter_database.order_content where order_code = '"+OrderId+"' and book_code = '"+obj.BookCode+"'";
+            Classes.ConnectionToDataBase.QueryExecution(DeleteBookQuery);
+            string UpdateCostOrder = "update bookhunter_database.order set order_cost = order_cost - " + obj.BookCost +" where order_code = '" + OrderId + "'";
+            Classes.ConnectionToDataBase.QueryExecution(UpdateCostOrder);
+            MySqlConnection connection = Classes.ConnectionToDataBase.Connection();
+            connection.Open();
+            string QueryCostOrder = "SELECT * FROM bookhunter_database.order where order_code = " + OrderId;
+            MySqlCommand command = new MySqlCommand(QueryCostOrder, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            CostOrder.Content = "Сумма: " + reader.GetString(2) + " руб.";
+            connection.Close();
+            OrderContent.ItemsSource = Classes.ConnectionToDataBase.OrderItems(OrderId);
+            if (OrderContent.Items.Count == 0)
+            {
+                string DeleteOrder = "Delete from bookhunter_database.order where order_code = '" + OrderId + "'";
+                Classes.ConnectionToDataBase.QueryExecution(DeleteOrder);
+                MessageBox.Show("Корзина пуста! Добавьте в неё товар");
+                OrderId = 0.ToString();
+                Catalog catalog = new Catalog(OrderId);
+                catalog.Show();
+                this.Close();
+            }
+        }
+        private void PaymentButton_Click(object sender, RoutedEventArgs e)
+        {
+            PaymentSuccesfull.Visibility = Visibility.Visible;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(OrderId);
+            Catalog catalog = new Catalog(OrderId);
+            catalog.Show();
+            this.Close();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string DeleteBookQuery = "Delete from bookhunter_database.order_content where order_code = '" + OrderId + "'";
+            Classes.ConnectionToDataBase.QueryExecution(DeleteBookQuery);
+            string DeleteOrder = "Delete from bookhunter_database.order where order_code = '" + OrderId + "'";
+            Classes.ConnectionToDataBase.QueryExecution(DeleteOrder);
+            StartWindow startWindow = new StartWindow();
+            startWindow.Show();
+            this.Close();
+        }
+    }
+}
